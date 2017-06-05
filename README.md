7ddb7ebe27a1f3f7c6b50a31c6fc3bf218cdf879
Lhr <1420432344@qq.com>
2017-06-05 10:55:12 +0800

    add Layered

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # TaoTaoShop
 > 参照传智播客的淘淘商城项目编写的基于Spring，Spring MVC，MyBatis，Redis，Solr的商城项目
 
-#### 1.了解分布式架构及其产生原因，构建Maven项目，并用MyBatis逆向工程长生POJO与DAO接口
+* 了解分布式架构及其产生原因，构建Maven项目，并用MyBatis逆向工程长生POJO与DAO接口
+
+
+* 进行Spring，SpringMVC，MyBatis框架整合，并对项目进行分层（DAO,Service，Controller），使用MyBatis进行商品展示页面分页。
+
 
 
 
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -5,17 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.pojo.EUDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbItemExample;
 import com.taotao.pojo.TbItemExample.Criteria;
 import com.taotao.service.ItemService;
+
 @Service
-public class ItemServiceImpl implements ItemService{
-	
+public class ItemServiceImpl implements ItemService {
+
 	@Autowired
 	private TbItemMapper itemMapper;
 	
+	/**
+	 * 单个商品查询
+	 */
 	@Override
 	public TbItem getItemById(long itemId) {
 		TbItemExample example = new TbItemExample();
@@ -27,6 +35,25 @@ public class ItemServiceImpl implements ItemService{
 			return item;
 		}
 		return null;
+	}
+
+	/**
+	 * 商品列表查询
+	 */
+	@Override
+	public EUDataGridResult getItemList(int page, int rows) {
+		// 查询商品
+		TbItemExample example = new TbItemExample();
+		// 分页处理
+		PageHelper.startPage(page, rows);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		// 创建一个返回值对象
+		EUDataGridResult result = new EUDataGridResult();
+		result.setRows(list);
+		//取记录总条数
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		result.setTotal(pageInfo.getTotal());
+		return result;
 	}
 
 }
