b03d7ab1adb0f2bf3bc1f506c7130c4c319fd95e
Lhr <1420432344@qq.com>
2017-06-18 22:29:44 +0800

    add shopcart

--- taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.taotao.sso.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -73,7 +77,12 @@ public class UserController {
 			return result;
 		}
 	}
-
+	
+	/**
+	 * 创建用户
+	 * @param user
+	 * @return
+	 */
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	@ResponseBody
 	public TaotaoResult createUser(TbUser user) {
@@ -85,5 +94,70 @@ public class UserController {
 			e.printStackTrace();
 			return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
 		}
+
 	}
+
+	/**
+	 * 用户登陆
+	 * 
+	 * @param username
+	 * @param password
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@ResponseBody
+	public TaotaoResult userLogin(String username, String password, HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			TaotaoResult result = userService.userLogin(username, password, request, response);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+	}
+	
+	@RequestMapping("/token/{token}")
+	@ResponseBody
+	public Object getUserByToken(@PathVariable String token,String callback){
+		TaotaoResult result = null;
+		try {
+			result = userService.getUserByToken(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+		//判断是否为jsoup调用
+		if (StringUtils.isBlank(callback)) {
+			return result;
+		}else {
+			MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+			mappingJacksonValue.setJsonpFunction(callback);
+			return result;
+		}
+	}
+	
+	@RequestMapping("/logout/{token}")
+	@ResponseBody
+	public TaotaoResult deleteUserByToken(@PathVariable String token,String callback,HttpServletRequest request,
+			HttpServletResponse response){
+		TaotaoResult result = null;
+		try {
+			result = userService.deleteUserByToken(token, request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+		//判断是否为JSOUP调用
+		if (StringUtils.isBlank(callback)) {
+			return result; 
+		}else {
+			MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+			mappingJacksonValue.setJsonpFunction(callback);
+			return result;
+		}
+	}
+	
 }
